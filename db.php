bbc83de1051dc839c7ba922fc4e89e4cdd036182
Sarvesh Kesharwani <sarveshkumar5513@gmail.com>
2018-04-21 14:28:11 +0530

    v1
    First commit

---
+++ db.php
@@ -0,0 +1,7 @@
+<?php
+/* Database connection settings */
+$host = 'sql205.byetcluster.com';
+$user = 'epiz_21954157';
+$pass = 'wV4FNMN2gzip';
+$db = 'epiz_21954157_accounts';
+$mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
